fc6a85625fe4008920b934288fe734950f04b32c
Breno <br3noacioli@gmail.com>
2023-05-19 15:59:12 -0300

    solved problems 01-04

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,11 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    if (num < 0) return "ERROR";
+    let sum = string;
+    for (let i = 0; i < num - 1; i++)
+    {
+        sum = sum + string;
+    }
+    return sum;
 };
 
 // Do not edit below this line
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function(Arr, ...num) {
+    const newArr = [];
+    for (i = 0; i < Arr.length; i++){
+        if (!num.includes(Arr[i])) {
+            newArr.push(Arr[i]);
+        }
+    }
+    return newArr;
+//esse metodo ficou diferente da solucao que usa forEach, mas passou em todos os testes
 
 };
 
