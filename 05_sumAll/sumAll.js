2d14367ea3f46e85b60a80030145551916a88db7
Breno <br3noacioli@gmail.com>
2023-05-19 16:57:09 -0300

    solved problem 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
+const sumAll = function(n1, n2) {
+    if (n1 < 0 || n2 < 0 || typeof n1 != "number" || typeof n2 != "number") {
+        return "ERROR";
+    }
+    if (n1 > n2) {
+        const temp = n1;
+        n1 = n2;
+        n2 = temp;  
+    }
 
+    let sum = 0;
+    for (let i = n1; i <= n2; i++){
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
