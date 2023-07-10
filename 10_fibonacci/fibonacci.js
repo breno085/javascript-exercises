286c6fb78409be304d07e044a45b2b9811b8f26f
Breno Acioli <br3noacioli@gmail.com>
2023-07-10 20:27:59 -0300

    Done with fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,14 @@
-const fibonacci = function() {
+const fibonacci = function(member) {
+    let pos = [0, 1, 1];
 
+    if (member < 0){
+        return "OOPS";
+    }
+    for (let i = 0; i <= member - 2; i++){
+        pos[i + 2] = pos[i + 1] + pos[i];
+    }
+    
+    return pos[member];
 };
 
 // Do not edit below this line
