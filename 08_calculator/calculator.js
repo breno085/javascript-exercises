62997f6028bd7379d5d707a7d4f145f71b35b5f1
Breno Acioli <br3noacioli@gmail.com>
2023-07-10 13:35:15 -0300

    done with calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,26 +1,36 @@
-const add = function() {
-	
+const add = function(a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  return arr.reduce((sum, item) => sum + item, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((mult, item) => mult*item, 1);
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+  return a**b;
 };
 
-const factorial = function() {
-	
+const factorial = function(numb) {
+  let fac = numb;
+  if (fac === 0)
+  return 1;
+  else {
+  for (i = 1; i < numb; i++){
+    fac = fac*(numb - i);
+    }
+  }
+  return fac;
 };
+
+
 
 // Do not edit below this line
 module.exports = {
