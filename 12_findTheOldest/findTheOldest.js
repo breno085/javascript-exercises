9f96ff0af8d8c5d9084a863eddc2ff35e42ba3a8
Breno Acioli <br3noacioli@gmail.com>
2023-07-10 23:00:15 -0300

    finished the last exercise 'findTheOldest'

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,13 @@
-const findTheOldest = function() {
+const findTheOldest = function(arr) {
+    const currentYear = new Date().getFullYear();
 
-};
+    return arr.reduce((oldest, person) => {
+        if (!person.yearOfDeath){
+            person.yearOfDeath = currentYear;
+        }
+        return (person.yearOfDeath - person.yearOfBirth) > (oldest.yearOfDeath - oldest.yearOfBirth) ? person : oldest;
+    }, arr[0]);
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
